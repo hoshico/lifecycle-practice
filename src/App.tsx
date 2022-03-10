88cd2c1952dc3813cfd045f6a385f9f8b359bcee
hoshico <melancholinista.yasu@gmail.com>
2022-03-11 08:11:54 +0900

    add loading

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,17 @@ import { UserCard } from './components/UserCard'
 import { User } from './types/api/user'
 import { UserProfile } from './types/userprofile'
 
-
 export default function App() {
-  const [ userProfiles, setUserProfiles ] = useState<Array<UserProfile>>([]);
-  const [ loading, setLoading ] = useState<boolean>(false);
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  // errorがあるのかどうかの判定
+  const [error, setError] = useState<boolean>(false)
+
   const onClickFetchUser = () => {
+    // onClickしたタイミングでloadingはtrueに
+    setLoading(true)
+    // 関数実行時は判定でerrorは無しにする
+    setError(false)
     axios
       .get<Array<User>>('https://jsonplaceholder.typicode.com/users')
       .then((res) => {
@@ -18,18 +24,35 @@ export default function App() {
           id: user.id,
           name: `${user.name}(${user.username})`,
           email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
         }))
-        setUserProfiles(data);
-      });
-  };
+        setUserProfiles(data)
+      })
+      .catch(() => {
+        // errorがあった場合にtrueにする
+        setError(true);
+      })
+      .finally(() => {
+        // 正常でもerrorでもloadingは終了
+        setLoading(false)
+      })
+  }
 
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      <br />
+      {error ? (
+        <p style={{color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   )
 }
