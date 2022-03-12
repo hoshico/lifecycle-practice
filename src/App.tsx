c2b580db215cb450782f1f3b424fd16a242aa145
hoshico <melancholinista.yasu@gmail.com>
2022-03-12 16:09:19 +0900

    add change textButton

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { UserCard } from './components/UserCard'
 
 
 export default function App() {
+  const [status, setStatus] = useState<string>("hello");
   const { getUsers, userProfiles, loading, error } = useAllUsers();
-
   const onClickFetchUser = () => getUsers()
 
   return (
     <div className="App">
+      <h4>{status}</h4>
+      <button onClick={() => setStatus('new text')}>change text</button>
       <button onClick={onClickFetchUser}>データ取得</button>
       <br />
       {error ? (
